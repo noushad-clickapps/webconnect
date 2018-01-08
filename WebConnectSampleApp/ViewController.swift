c1713db21d842113befd04c168860cb22d45e229
noushad-clickapps <noushad@clickapps.co>
2018-01-08 16:45:58 +0530

    committing code

--- WebConnectSampleApp/ViewController.swift
+++ WebConnectSampleApp/ViewController.swift
@@ -53,8 +53,7 @@ class ViewController: UIViewController, Callback {
         
         WebConnect.get()
             .url(url: "offers")
-           // .header(header: ["Test": "Header"])
-           // .queryParam(queryParam: ["name":"Hello"])
+          
 
             .callback(callback: self)
             .tag(tag: 1)
